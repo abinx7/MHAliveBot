d5e3a9988267a78757925b2029c9db1035b8c573
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-04 15:54:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -570,9 +570,9 @@ async def auto_filter(client, msg):
         else:
              return            
     if settings["button"]:
-        btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}')], for file in files ]               
+        btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}')] for file in files ]               
     else:
-        btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}'), InlineKeyboardButton(text=f"[{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}')], for file in files ]                 
+        btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}'), InlineKeyboardButton(text=f"[{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}')] for file in files ]                 
                                
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
