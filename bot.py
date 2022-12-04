df81d881018e3997ee89238a04d0c1449292fba0
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-04 14:53:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,3 @@
-import logging
-import logging.config
-
-# Get logging configurations
-#logging.config.fileConfig('logging.conf')
-#logging.getLogger().setLevel(logging.INFO)
-#logging.getLogger("pyrogram").setLevel(logging.ERROR)
-#logging.getLogger("imdbpy").setLevel(logging.ERROR)
-
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from aiohttp import web
@@ -26,7 +17,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=500,
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
@@ -59,30 +50,7 @@ class Bot(Client):
         chat_id: Union[int, str],
         limit: int,
         offset: int = 0,
-    ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
+    ) -> Optional[AsyncGenerator["types.Message", None]]:       
         current = offset
         while True:
             new_diff = min(200, limit - current)
