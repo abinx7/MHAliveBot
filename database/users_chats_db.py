54b2cf236bdc3594dbac50fe0538d290241ace5e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-06 20:58:41 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,146 +1,90 @@
-# https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
+from info import DATABASE_NAME, DATABASE_URI
 
 class Database:
-    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
-        self.grp = self.db.groups
 
-
-    def new_user(self, id, name):
+    def new_user(self, id):
         return dict(
-            id = id,
-            name = name,
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            notif=True,
             ban_status=dict(
                 is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
                 ban_reason="",
             ),
         )
 
-
-    def new_group(self, id, title):
-        return dict(
-            id = id,
-            title = title,
-            chat_status=dict(
-                is_disabled=False,
-                reason="",
-            ),
-        )
-    
-    async def add_user(self, id, name):
-        user = self.new_user(id, name)
+    async def add_user(self, id):
+        user = self.new_user(id)
         await self.col.insert_one(user)
-    
+
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
-        return bool(user)
-    
+        user = await self.col.find_one({"id": int(id)})
+        return True if user else False
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-    
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({"id": int(user_id)})
+
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
-            ban_reason=''
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
         )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
-    
-    async def ban_user(self, user_id, ban_reason="No Reason"):
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason):
         ban_status = dict(
             is_banned=True,
-            ban_reason=ban_reason
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
         )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
 
     async def get_ban_status(self, id):
         default = dict(
             is_banned=False,
-            ban_reason=''
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
         )
-        user = await self.col.find_one({'id':int(id)})
-        if not user:
-            return default
-        return user.get('ban_status', default)
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("ban_status", default)
 
-    async def get_all_users(self):
-        return self.col.find({})
-    
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({"ban_status.is_banned": True})
+        return banned_users
 
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
+    async def set_notif(self, id, notif):
+        await self.col.update_one({"id": id}, {"$set": {"notif": notif}})
 
+    async def get_notif(self, id):
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("notif", False)
 
-    async def get_banned(self):
-        users = self.col.find({'ban_status.is_banned': True})
-        chats = self.grp.find({'chat_status.is_disabled': True})
-        b_chats = [chat['id'] async for chat in chats]
-        b_users = [user['id'] async for user in users]
-        return b_users, b_chats
-    
+    async def get_all_notif_user(self):
+        notif_users = self.col.find({"notif": True})
+        return notif_users
 
-
-    async def add_chat(self, chat, title):
-        chat = self.new_group(chat, title)
-        await self.grp.insert_one(chat)
-    
-
-    async def get_chat(self, chat):
-        chat = await self.grp.find_one({'id':int(chat)})
-        return False if not chat else chat.get('chat_status')
-    
-
-    async def re_enable_chat(self, id):
-        chat_status=dict(
-            is_disabled=False,
-            reason="",
-            )
-        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
-        
-    async def update_settings(self, id, settings):
-        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
-        
-    
-    async def get_settings(self, id):
-        default = {
-            'button': SINGLE_BUTTON,
-            'botpm': P_TTI_SHOW_OFF,
-            'file_secure': PROTECT_CONTENT,
-            'imdb': None,
-            'spell_check': SPELL_CHECK_REPLY,
-            'welcome': MELCOW_NEW_USERS,
-            'template': None
-        }
-        chat = await self.grp.find_one({'id':int(id)})
-        if chat:
-            return chat.get('settings', default)
-        return default
-    
-
-    async def disable_chat(self, chat, reason="No Reason"):
-        chat_status=dict(
-            is_disabled=True,
-            reason=reason,
-            )
-        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
-    
-
-    async def total_chat_count(self):
-        count = await self.grp.count_documents({})
+    async def total_notif_users_count(self):
+        count = await self.col.count_documents({"notif": True})
         return count
-    
-
-    async def get_all_chats(self):
-        return self.grp.find({})
-
-
-    async def get_db_size(self):
-        return (await self.db.command("dbstats"))['dataSize']
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
