191ef62a2c7628a242b16ca21d616dbbd9ce058c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-06 22:23:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,24 +1,17 @@
-import re
-from os import environ
-id_pattern = re.compile(r'^.\d+$')
-
 # Bot information
-SESSION = "Media_search"
 API_ID = int(7538843)
 API_HASH = "9c1f4ab1c1db2ebdb18d48e4257d2074"
 BOT_TOKEN = "2077834181:AAFUUxI5vtSg1YXte-xJlzzTKuD4_k2-s_0"
 
 # Bot settings
-CACHE_TIME = int(300)
 USE_CAPTION_FILTER = True 
 PICS = ["https://graph.org/file/6b5b947f67b2a04977e31.jpg"]
 
 # Admins, Channels & Users
 ADMINS = [1872853375, 1993953092]
 CHANNELS = int(-1001771962158)
-AUTH_USERS = ADMINS
 AUTH_CHANNEL = int(-1001870323556)
-AUTH_GROUPS = None
+
 
 # MongoDB information
 DATABASE_URI = "mongodb+srv://tinson:tinson@cluster0.x6pcf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -40,10 +33,4 @@ Join ⚡️
 <a href=https://t.me/TinsonTs>[OTT MOVIES]</a>
 <a href=https://t.me/TinsonTs>[CONFERENCE HALL]</a>"""
 
-
-P_TTI_SHOW_OFF = True 
-SINGLE_BUTTON = True 
 SPELL_CHECK_REPLY = False
-MELCOW_NEW_USERS = True 
-PROTECT_CONTENT = False 
-PUBLIC_FILE_STORE = False
